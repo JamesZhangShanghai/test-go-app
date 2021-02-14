4f67ab29f65643b1f02a15666cb94b16a4f7ce4a
jameszhangcn <samoren@126.com>
2021-02-14 23:17:26 +0800

    test

--- Jenkinsfile.sh
+++ Jenkinsfile.sh
@@ -8,7 +8,7 @@ pipeline {
                 sh 'export GO111MODULE=on'
                 sh 'export GOPROXY=https://goproxy.io'
                 script {
-                    sh "sleep 180"
+                    sh "sleep 1800"
                     if( sh(script: 'docker run --rm -v $(pwd):/go/src/gowebdemo -w /go/src/gowebdemo golang:1.14.0 /bin/sh -c "/go/src/gowebdemo/rununittest.sh"', returnStatus: true ) != 0 ){
                        currentBuild.result = 'FAILURE'
                     }
