1a1f2b3c0c8c2ba42eecc96fa527876350c6798b
jameszhangcn <samoren@126.com>
2021-02-14 23:41:38 +0800

    test

--- rununittest.sh
+++ rununittest.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 echo "go unit test"
-export GO111MODULE=on
-export GOPROXY=https://goproxy.cn
-sleep 1800
 set -x
-
+go mod init goapp
 go get -d -v golang.org/x/net/html
 go get -u github.com/jstemmer/go-junit-report
 go test -v 2>&1 > tmp
