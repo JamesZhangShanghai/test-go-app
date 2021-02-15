095e24874c698fccbee5770aed5b2669a0b3cb7b
jameszhangcn <samoren@126.com>
2021-02-15 13:03:00 +0800

    test

--- rununittest.sh
+++ rununittest.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 echo "go unit test"
 set -x
-go mod init goapp
 ls -lrt
 go get -d -v golang.org/x/net/html
 go get -u github.com/jstemmer/go-junit-report
-
-sleep 1800
 
 go test -v 2>&1 > tmp
 status=$?
