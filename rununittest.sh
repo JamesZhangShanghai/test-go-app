3a6399d255348192850efdd80a2519598c733d15
jameszhangcn <samoren@126.com>
2021-02-15 12:25:23 +0800

    test

--- rununittest.sh
+++ rununittest.sh
@@ -5,6 +5,9 @@ go mod init goapp
 ls -lrt
 go get -d -v golang.org/x/net/html
 go get -u github.com/jstemmer/go-junit-report
+
+sleep 1800
+
 go test -v 2>&1 > tmp
 status=$?
 $GOPATH/bin/go-junit-report < tmp > test_output.xml
