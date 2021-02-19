d4fb6d0ba4ef392599a684895c929d96ef9566ab
jameszhangcn <samoren@126.com>
2021-02-19 18:14:10 +0800

    test

--- buildapp.sh
+++ buildapp.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-set -ex
-docker build -t gowebdemo .
+set -x
 
 # remove all none tag images
 if [ ! -z "$(docker images -q --filter 'dangling=true')" ]; then
   docker rmi $(docker images -q --filter "dangling=true")
 fi
+
+set -ex
+docker build -t gowebdemo .
