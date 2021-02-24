be507ead7f4fba29f20a0a2ea4a74f6fd96480eb
jameszhangcn <samoren@126.com>
2021-02-24 18:40:24 +0800

    test

--- buildapp.sh
+++ buildapp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 set -x
-export REGISTRY_ADDR = '192.168.56.210:30500'
+export REGISTRY_ADDR="192.168.56.210:30500"
 # remove all none tag images
 if [ ! -z "$(docker images -q --filter 'dangling=true')" ]; then
   docker rmi $(docker images -q --filter "dangling=true")
