cf108b7633a37fbecfcb6011affc56fed94f1873
jameszhangcn <samoren@126.com>
2021-03-01 11:19:17 +0800

    test

--- rununittest.sh
+++ rununittest.sh
@@ -5,10 +5,11 @@ ls -lrt
 go get -d -v golang.org/x/net/html
 go get -u github.com/jstemmer/go-junit-report
 
-mkdir report
+mkdir ./report
+chmod 777 ./report
+ls -lrt
 go test -v 2>&1 > tmp
 status=$?
 $GOPATH/bin/go-junit-report < tmp > ./report/index.xml
 cat ./report/index.xml
-sleep 300
 exit ${status}
