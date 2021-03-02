fadde0528445e53b706fd36113ba18d6b076b122
jameszhangcn <samoren@126.com>
2021-03-02 15:35:05 +0800

    test

--- Jenkinsfile.sh
+++ Jenkinsfile.sh
@@ -17,7 +17,7 @@ pipeline {
                     sh "echo ${env.BUILD_ID}"
                     sh "mkdir -p /home/workspace/${env.BUILD_TAG}"
                     sh "cp -rf ./* /home/workspace/${env.BUILD_TAG}"
-                    if( sh(script: 'docker run -e "GO111MODULE=on" -e "GOPROXY=https://goproxy.cn" --rm -v $(pwd):/go/src/gowebdemo -w /go/src/gowebdemo golang:1.14.0 /bin/sh -c "/go/src/gowebdemo/rununittest.sh"', returnStatus: true ) != 0 ){
+                    if( sh(script: 'docker run -e "GO111MODULE=on" -e "GOPROXY=https://goproxy.cn" --rm -v /home/workspace/${env.BUILD_TAG}:/go/src/gowebdemo -w /go/src/gowebdemo golang:1.14.0 /bin/sh -c "/go/src/gowebdemo/rununittest.sh"', returnStatus: true ) != 0 ){
                        currentBuild.result = 'FAILURE'
                     }
                 }
