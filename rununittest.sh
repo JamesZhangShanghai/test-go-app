9d04fbd550a8e1eb4e1f114c4095608bbf70d17b
jameszhangcn <samoren@126.com>
2021-03-21 09:06:52 +0800

    add gcflags

--- rununittest.sh
+++ rununittest.sh
@@ -8,7 +8,7 @@ go get -u github.com/jstemmer/go-junit-report
 mkdir ./report
 chmod 777 ./report
 ls -lrt
-go test -coverprofile=coverage.data -timeout=5s -v 2>&1 > tmp
+go test -coverprofile=coverage.data -timeout=5s -gcflags=-l -v 2>&1 > tmp
 status=$?
 go tool cover -html=coverage.data -o ./report/coverage.html
 $GOPATH/bin/go-junit-report < tmp > ./report/index.xml
